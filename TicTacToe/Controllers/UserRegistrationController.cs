2d913ad113479976d979878ee6fcfe81048f72a4
Wayson Lin <wayson317@msn.com>
2018-02-11 20:18:23 +0000

    More commit

--- TicTacToe/Controllers/UserRegistrationController.cs
+++ TicTacToe/Controllers/UserRegistrationController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Models;
+using TicTacToe.Services;
 
 namespace TicTacToe.Controllers
 {
     public class UserRegistrationController : Controller
     {
+        private IUserService _userService;
+
+        public UserRegistrationController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel userModel)
+        {
+            await _userService.RegisterUser(userModel);
+
+            return Content($"User {userModel.FirstName} {userModel.LastName} has been registered successfully");
         }
     }
 }
--- TicTacToe/Startup.cs
+++ TicTacToe/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TicTacToe.Services;
 
 namespace TicTacToe
 {
@@ -16,6 +17,7 @@ namespace TicTacToe
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
